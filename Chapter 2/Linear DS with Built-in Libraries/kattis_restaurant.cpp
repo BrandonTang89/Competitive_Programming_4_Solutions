de6ee5ff0c3e5b33ba0f065d1007e6fab2b90345
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2021-09-23 11:51:26 +0800

    Add files via upload

---
+++ Chapter 2/Linear DS with Built-in Libraries/kattis_restaurant.cpp
@@ -0,0 +1,53 @@
+/*
+Kattis - Restaurant
+A relatively easy question (that i somehow found annoyingly hard to debug)
+
+Time: O(TC n), Mem: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+long long n, m, s1_c, s2_c;
+string op;
+int main(){
+    
+    while (true){
+        cin >> n;
+        //cout << "n " << n << endl;
+        if (n==0)break;
+        s1_c = s2_c = 0;
+        for (int e=0;e<n;e++){
+            cin >> op >> m;
+            if (op == "DROP"){
+                printf("DROP 2 %lld\n", m); 
+                s2_c += m;
+
+            }
+            else if (op == "TAKE"){
+                if (s1_c >= m){
+                    s1_c -= m;
+                    printf("TAKE 1 %lld\n", m);
+                }
+                else{
+                    if (s1_c != 0){ 
+                        printf("TAKE 1 %lld\n", s1_c);
+                        m -= s1_c;
+                        s1_c = 0;
+                    }
+
+                    printf("MOVE 2->1 %lld\n", s2_c);
+                    s1_c = s2_c - m;
+                    s2_c = 0;
+                    printf("TAKE 1 %lld\n", m);
+                    
+                }
+
+            }
+        }
+    
+        printf("\n");
+        
+    }
+    
+}
