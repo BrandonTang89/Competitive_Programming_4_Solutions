6ee2d54c153c8f37564583624d0865cfb37110c5
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2021-10-01 07:35:24 +0800

    Add files via upload

--- Chapter_3/Complete_Search/kattis_communication.cpp
+++ Chapter_3/Complete_Search/kattis_communication.cpp
@@ -1,7 +1,16 @@
+/* Kattis - Communication
+
+Observation 1:
+    there are much more bytes to process than number of unique bytes. So instead of trying to reverse all the bytes
+    in the data stream one by one, we compute all possible encrypted bytes and store it into a hash table which can then be
+    used to decrypt the steam bytes in O(1) time for each byte
+
+Time: O(n), Mem: O(n)
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
-map<int, int> m;  // m[hashed] = byte
+unordered_map<int, int> m;  // m[hashed] = byte
 int n, h;
 
 int main() {
