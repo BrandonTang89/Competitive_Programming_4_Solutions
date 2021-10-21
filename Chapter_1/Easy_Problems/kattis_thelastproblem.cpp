844fb3b1e919522a6db98c32b4aca09b0e396a07
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2021-10-21 10:32:04 +0800

    Create kattis_thelastproblem.cpp

---
+++ Chapter_1/Easy_Problems/kattis_thelastproblem.cpp
@@ -0,0 +1,14 @@
+/* Kattis - thelastproblem
+Basic IO, getline
+
+Time: O(1), Mem: O(1)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+string s;
+int main(){
+  getline(cin, s);
+  cout << "Thank you, " << s << ", and farewell!" << endl;
+  return 0;
+}
