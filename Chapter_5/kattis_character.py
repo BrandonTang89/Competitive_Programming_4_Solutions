fa39a4f7a51835e51b762a9023d4c87186ce9971
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2021-11-18 14:54:03 +0800

    Delete kattis_character.py

--- Chapter_5/kattis_character.py
+++
@@ -1,8 +0,0 @@
-'''
-Kattis - character
-Return the number of subsets with >= 2 characters. Ie total number of subsets - subsets with 1 char
-and subsets with 0 char
-Time: O(log n), Space: O(1)
-'''
-n = int(input())
-print(2**n - n - 1)
