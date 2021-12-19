1ee5dc4f74fc435aef6cbe351bceb73ebc5975c5
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2021-12-19 20:43:19 +0800

    Add files via upload

---
+++ Chapter_3/Complete_Search/kattis_set.py
@@ -0,0 +1,21 @@
+'''
+Kattis - set
+Mostly reading comprehension...
+
+Time: O(1), Space: O(1)
+'''
+arr = []
+for i in range(4):
+    arr.extend(list(input().split()))
+
+for i in range(12):
+    for j in range(i+1, 12):
+        for k in range(j+1, 12):
+            for l in range(4):
+                if (arr[i][l] == arr[j][l] and arr[i][l] == arr[k][l]):
+                    continue
+                if (arr[i][l] == arr[j][l] or arr[i][l] == arr[k][l] or arr[j][l] == arr[k][l]):
+                    break
+            else:
+                print(i+1, j+1, k+1)
+                
