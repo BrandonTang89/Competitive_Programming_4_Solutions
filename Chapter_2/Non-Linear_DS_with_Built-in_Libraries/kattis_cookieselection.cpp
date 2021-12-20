02d244f4d0ebad8069619d357cd6b0a6f5daf7ae
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2021-12-20 09:44:47 +0800

    Add files via upload

---
+++ Chapter_2/Non-Linear_DS_with_Built-in_Libraries/kattis_cookieselection.cpp
@@ -0,0 +1,34 @@
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+
+#include <bits/extc++.h>
+using namespace __gnu_pbds;
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update> ost;
+
+string s;
+ost T;
+ll n, pos;
+int main() {
+    while (cin >> s){
+        if (s != "#"){
+            T.insert(stoll(s));
+        }
+        else{
+            n = T.size();
+            if (n&1){
+                pos = (n-1)/2;
+            }
+            else{
+                pos = n/2;
+            }
+            cout << *T.find_by_order(pos) << endl;
+            T.erase(T.find_by_order(pos));
+        }
+    }
+    return 0; 
+}
