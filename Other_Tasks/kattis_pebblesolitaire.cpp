f850ef45ee417ddb95d7d88dabe714aa8b8f5227
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2022-03-26 10:38:56 +0800

    Add files via upload

---
+++ Other_Tasks/kattis_pebblesolitaire.cpp
@@ -0,0 +1,47 @@
+/**Kattis - pebblesolitaire
+ * Easier version of pebblesolitaire2
+ * 
+ * Time: O(12 * 2^12), Space: O(2^12)
+ */
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+using namespace std;
+
+int memo[(1 << 12) + 9];
+int dp(int mask) {  // minimum possible number of pebbles after an arbitary number of moves
+    if (memo[mask] != -1) return memo[mask];
+
+    int ans = __builtin_popcount(mask);
+    for (int i = 0; i < 12; i++) {
+        if (mask & (1 << i)) {
+            // try to move left and move right
+            if (i >= 2 && (mask & (1 << (i - 1))) && (!(mask & (1 << (i - 2))))) {
+                ans = min(ans, dp(mask ^ (1 << i) ^ (1 << (i - 1)) ^ (1 << (i - 2))));
+            }
+            if (i <= 9 && (mask & (1 << (i + 1))) && (!(mask & (1 << (i + 2))))) {
+                ans = min(ans, dp(mask ^ (1 << i) ^ (1 << (i + 1)) ^ (1 << (i + 2))));
+            }
+        }
+    }
+    return memo[mask] = ans;
+}
+int main() {
+    int n;
+    string s;
+    cin >> n;
+    memset(memo, -1, sizeof memo);
+    for (int i = 0; i < n; i++) {
+        cin >> s;
+        int bm = 0;
+        for (auto c : s) {
+            bm *= 2;
+            bm += (c == 'o');
+        }
+
+        cout << dp(bm) << endl;
+    }
+
+    return 0;
+}
