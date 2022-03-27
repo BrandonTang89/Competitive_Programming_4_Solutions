0b8ad323b046002e8aaa725229c23aaa869d121e
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2022-03-27 10:28:46 +0800

    Add files via upload

---
+++ Chapter_8/8.4/kattis_mazemovement.cpp
@@ -0,0 +1,106 @@
+/**Kattis - mazemovement
+ * Pretty easy max flow graph modelling. Each edge is bidirectional, has a capacity of gcd(u, v) if u and v are
+ * not relatively prime, else 0. We just want the max flow from the smallest room to the largest room.
+ * 
+ * Time: O(V^2 * E), Space: O(V + E)
+ */
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>  
+using namespace std;
+
+#define fast_cin() ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+typedef long long ll;
+typedef tuple<int, ll, ll> edge;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+const ll INF = 1e18;  // large enough
+
+class max_flow {
+   private:
+    int V;
+    vector<edge> EL;
+    vector<vi> AL;
+    vi d, last;
+    vector<ii> p;
+
+    bool BFS(int s, int t) {  // find augmenting path
+        d.assign(V, -1);
+        d[s] = 0;
+        queue<int> q({s});
+        p.assign(V, {-1, -1});  // record BFS sp tree
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            if (u == t) break;                                    // stop as sink t reached
+            for (auto &idx : AL[u]) {                             // explore neighbors of u
+                auto &[v, cap, flow] = EL[idx];                   // stored in EL[idx]
+                if ((cap - flow > 0) && (d[v] == -1))             // positive residual edge
+                    d[v] = d[u] + 1, q.push(v), p[v] = {u, idx};  // 3 lines in one!
+            }
+        }
+        return d[t] != -1;  // has an augmenting path
+    }
+
+    ll DFS(int u, int t, ll f = INF) {  // traverse from s->t
+        if ((u == t) || (f == 0)) return f;
+        for (int &i = last[u]; i < (int)AL[u].size(); ++i) {  // from last edge
+            auto &[v, cap, flow] = EL[AL[u][i]];
+            if (d[v] != d[u] + 1) continue;  // not part of layer graph
+            if (ll pushed = DFS(v, t, min(f, cap - flow))) {
+                flow += pushed;
+                auto &rflow = get<2>(EL[AL[u][i] ^ 1]);  // back edge
+                rflow -= pushed;
+                return pushed;
+            }
+        }
+        return 0;
+    }
+
+   public:
+    max_flow(int initialV) : V(initialV) {
+        EL.clear();
+        AL.assign(V, vi());
+    }
+
+    // if you are adding a bidirectional edge u<->v with weight w into your
+    // flow graph, set directed = false (default value is directed = true)
+    void add_edge(int u, int v, ll w, bool directed = true) {
+        if (u == v) return;                       // safeguard: no self loop
+        EL.emplace_back(v, w, 0);                 // u->v, cap w, flow 0
+        AL[u].push_back(EL.size() - 1);           // remember this index
+        EL.emplace_back(u, directed ? 0 : w, 0);  // back edge
+        AL[v].push_back(EL.size() - 1);           // remember this index
+    }
+
+    ll dinic(int s, int t) {
+        ll mf = 0;                    // mf stands for max_flow
+        while (BFS(s, t)) {           // an O(V^2*E) algorithm
+            last.assign(V, 0);        // important speedup
+            while (ll f = DFS(s, t))  // exhaust blocking flow
+                mf += f;
+        }
+        return mf;
+    }
+};
+int main(){
+    int n;
+    cin >> n;
+    vector<int> rooms(n);
+    for (int i=0; i<n; i++){
+        cin >> rooms[i];
+    }
+    sort(rooms.begin(), rooms.end());
+    max_flow G(n);
+    for (int i=0; i<n; i++){
+        for (int j=i+1; j<n; j++){
+            if (gcd(rooms[i], rooms[j]) != 1){
+                G.add_edge(i, j, gcd(rooms[i], rooms[j]), false);
+            }
+        }
+    }
+    cout << G.dinic(0, n-1) << endl;
+
+    return 0;
+}
