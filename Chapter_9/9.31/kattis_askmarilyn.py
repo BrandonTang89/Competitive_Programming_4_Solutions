b59b8033e0b8b75b5349976efa4ba357959b2366
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2022-05-09 10:25:33 +0800

    Add files via upload

--- Chapter_9/9.31/kattis_askmarilyn.py
+++ Chapter_9/9.31/kattis_askmarilyn.py
@@ -1,3 +1,10 @@
+'''
+Kattis - askmarilyn
+Monty hall but abit modified, also interactive. Effectively we are betting that we select the wrong door initially,
+this occurs 2/3 of the time, then if she opens the bottle, go there, else swap. Either way, we get the bottle. But if we
+select the right door initially, we're gonna lose that bottle.
+
+Time: O(1), Space: O(1)'''
 from random import choice
 doors = ['A','B','C']
 for _ in range(1000): # 1000 reounds
