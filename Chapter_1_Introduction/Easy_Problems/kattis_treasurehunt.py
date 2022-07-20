feedbf14766cad9a17fb198283828c0d1299a72e
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2022-07-20 22:45:22 +0800

    Add files via upload

---
+++ Chapter_1_Introduction/Easy_Problems/kattis_treasurehunt.py
@@ -0,0 +1,31 @@
+h, w = map(int, input().split())
+grid = [list(input()) for _ in range(h)]
+
+past = set()
+r = 0
+c = 0
+x = 0
+while 1:
+    if (grid[r][c] == 'T'):
+        print(x)
+        exit()
+    
+    if ((r, c) in past): 
+        print("Lost")
+        exit()
+    
+    past.add((r, c))
+    if (grid[r][c] == 'E'):
+        c += 1
+    elif (grid[r][c] == 'N'):
+        r -= 1
+    elif (grid[r][c] == 'S'):
+        r += 1
+    elif (grid[r][c] == 'W'):
+        c -= 1
+    
+    if (r < 0 or r >= h or c < 0 or c >= w):
+        print("Out")
+        exit()
+    
+    x += 1
