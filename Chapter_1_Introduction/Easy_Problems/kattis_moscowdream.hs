d98c3e48f5100fc86a289ebadc1f608b4f3907d1
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2022-07-28 10:05:41 +0800

    Add files via upload

---
+++ Chapter_1_Introduction/Easy_Problems/kattis_moscowdream.hs
@@ -0,0 +1,11 @@
+main :: IO ()
+main = do
+    args <- getLine
+    let line = map read (words args) :: [Int]
+    let a = line !! 0
+    let b = line !! 1
+    let c = line !! 2
+    let n = line !! 3
+
+    let result = if (a <= 0 || b <= 0 || c <= 0 || (a+b+c) < n || n < 3) then "NO" else "YES"
+    putStrLn result
