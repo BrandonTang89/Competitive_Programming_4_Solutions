b63042cb40b1dbb16ab7c57a06d20fd332d3af2e
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2023-02-27 20:27:07 +0000

    Update kattis_smil.py

--- Other_Tasks/kattis_smil.py
+++ Other_Tasks/kattis_smil.py
@@ -1,5 +1,5 @@
 # Kattis -smil
-# Simplely find all occurances of the 4 types of substrings which don't overlap. This is done easily with regex.
+# Simply find all occurances of the 4 types of substrings which don't overlap. This is done easily with regex.
 # Time: O(n), Mem: O(n)
 import re
 for i in [m.start() for m in re.finditer(':\)|;\)|:-\)|;-\)', input())]:
