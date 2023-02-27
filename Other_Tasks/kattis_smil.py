9e9adb72b47e7097e7fd25a35a1a8ddd5ed9e8c8
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2023-02-27 20:26:47 +0000

    Add files via upload

---
+++ Other_Tasks/kattis_smil.py
@@ -0,0 +1,6 @@
+# Kattis -smil
+# Simplely find all occurances of the 4 types of substrings which don't overlap. This is done easily with regex.
+# Time: O(n), Mem: O(n)
+import re
+for i in [m.start() for m in re.finditer(':\)|;\)|:-\)|;-\)', input())]:
+    print(i)
