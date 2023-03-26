1db6d16992a5537c7790f4593171933b863f6633
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2023-03-26 14:35:38 +0800

    Add files via upload

---
+++ Chapter_8_Advanced_Topics/8.3_DP_Techniques/kattis_mailbox.py
@@ -0,0 +1,33 @@
+'''
+Kattis - mailbox
+The let dp(k, l, r) be the minimum cost with k mailboxes, and we need to test [l, r).
+We try all possible number of firecrackers within l, r and take the best position test.
+The recurrences are not exactly trivial to get right but the sample testcase is good
+to debug.
+
+Time: O(k*m^3)
+''' 
+from functools import lru_cache
+tc = int(input())
+
+
+@lru_cache(maxsize=None)
+def dp(k, l, r):  # could be [l, r) or not possible
+
+    if (k == 1):
+        return (sum(range(l, r)))
+    if (l == r):
+        return 0
+    if (l + 1 == r):
+        return l
+    assert (l < r)
+
+    ans = int(1e9)
+    for i in range(l, r):
+        ans = min(ans, i + max(dp(k-1, l, i), dp(k, i+1, r)))
+    return ans
+
+
+for _ in range(tc):
+    k, m = map(int, input().split())
+    print(dp(k, 1, m+1))
