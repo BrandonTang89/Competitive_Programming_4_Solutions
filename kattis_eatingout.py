43880d1578c3afbf4c6de4f3a25a091a22dab0d7
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2024-01-18 16:22:26 +0000

    Delete kattis_eatingout.py

--- kattis_eatingout.py
+++
@@ -1,9 +0,0 @@
-'''
-Kattis - eatingout
-We split each of the m options into 2 virtual items corresponding to the fact that each item can only be chosen by up to 2 people.
-Then we just check if it possible for each person to choose a disjoint set of these 2m virtual items.
-
-Time: O(1), Space: O(1)
-'''
-m, a, b, c = map(int, input().split())
-print('possible' if a+b+c <= 2*m else 'impossible')
