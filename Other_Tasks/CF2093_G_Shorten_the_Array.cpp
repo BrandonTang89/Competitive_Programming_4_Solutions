ff6bdecc74debb39b9f13bc8bdc0fa070eb5c2cc
Brandon Tang <43487872+BrandonTang89@users.noreply.github.com>
2025-04-09 16:20:45 +0100

    Update CF2093_G_Shorten_the_Array.cpp
    deal with hack

--- Other_Tasks/CF2093_G_Shorten_the_Array.cpp
+++ Other_Tasks/CF2093_G_Shorten_the_Array.cpp
@@ -8,7 +8,15 @@ using namespace std;
 
 int n, k;
 vector<int> a;
+unordered_map<int, int> last_seen[31];  // last_seen[i][j] is the last seen index of j (masking out the least significant i bits)
 int main() {
+    for (int i = 0; i < 31; i++) {
+        last_seen[i].reserve(1e4); // deal with the hacking issue...
+    }
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
     int tc;
     cin >> tc;
     while (tc--) {
@@ -38,16 +46,20 @@ int main() {
         }
 
         int best = 1e9;
-        unordered_map<int, int> last_seen[32];  // last_seen[i][j] is the last seen index of j (masking out the least significant i bits)
+        for (int i = 0; i < 31; i++) {
+            last_seen[i].clear();
+        }
+
         for (int i = 0; i < n; i++) {
             int looking_for = 0;
-            for (int j = 31; j >= 0; j--) {
+            for (int j = 30; j >= 0; j--) {
                 if ((1 << j) & k) {  // must follow along differently
                     looking_for = looking_for | ((1 << j) ^ ((1 << j) & a[i]));
                 } else {  // we can try to find someone who has a different version at this point
                     looking_for = looking_for | ((1 << j) ^ ((1 << j) & a[i]));
-                    if (last_seen[j].contains(looking_for)) {
-                        int idx = last_seen[j][looking_for];
+                    auto it = last_seen[j].find(looking_for);
+                    if (it != last_seen[j].end()) {
+                        int idx = it->second;
                         best = min(best, i - idx + 1);
                     }
                     // otherwise assume this bit will be the same
@@ -56,13 +68,14 @@ int main() {
             }
 
             // also check non-masked
-            if (last_seen[0].contains(looking_for)) {
-                int idx = last_seen[0][looking_for];
+            auto it = last_seen[0].find(looking_for);
+            if (it != last_seen[0].end()) {
+                int idx = it->second;
                 best = min(best, i - idx + 1);
             }
 
             int mask = ~0;
-            for (int j = 0; j < 32; j++) {
+            for (int j = 0; j < 31; j++) {
                 last_seen[j][a[i] & mask] = i;
                 mask = mask ^ (1 << j);
             }
